4419b47a61273a8c0499af942022ba0522d30e47
MatthewJohnson257 <MJohnson5342@gmail.com>
2020-02-15 13:52:20 -0500

    Added useful comments to explain things

--- board.py
+++ board.py
@@ -3,30 +3,52 @@
 from piece import Piece, Pawn, Rook, Knight, Bishop, Queen, King
 
 class Board():
+
+    # this is the constructor: it takes in 'grid', which is a 2d array of char
+    # then scans it for the values, and creates the appropriate 'Piece' object,
+    # but a blank space is given the value 'None', which is Python's version of
+    # null
+    # 
+    # For reference: 
+    #       - any method that says '__init__' is a constructor.
+    #       - 'self' just references to the specific object's instance, kind of
+    #           like Java's 'this'
     def __init__(self, grid):
         for i in grid:
             for j in grid[0]:
                 if(grid[i][j] == '-'):
                     grid[i][j] = None
                 elif(grid[i][j] == 'P'):
-                    grid[i][j] = Pawn(j, i, 'w')
+                    grid[i][j] = Pawn(i, j, 'w')
                 elif(grid[i][j] == 'p'):
-                    grid[i][j] = Pawn(j, i, 'b')
+                    grid[i][j] = Pawn(i, j, 'b')
                 elif(grid[i][j] == 'N'):
-                    grid[i][j] = Knight(j, i, 'w')
+                    grid[i][j] = Knight(i, j, 'w')
                 elif(grid[i][j] == 'n'):
-                    grid[i][j] = Knight(j, i, 'b')
+                    grid[i][j] = Knight(i, j, 'b')
                 elif(grid[i][j] == 'B'):
-                    grid[i][j] = Bishop(j, i, 'w')
+                    grid[i][j] = Bishop(i, j, 'w')
                 elif(grid[i][j] == 'b'):
-                    grid[i][j] = Bishop(j, i, 'b')
+                    grid[i][j] = Bishop(i, j, 'b')
                 elif(grid[i][j] == 'Q'):
-                    grid[i][j] = Queen(j, i, 'w')
+                    grid[i][j] = Queen(i, j, 'w')
                 elif(grid[i][j] == 'q'):
-                    grid[i][j] = Queen(j, i, 'b')
+                    grid[i][j] = Queen(i, j, 'b')
                 elif(grid[i][j] == 'K'):
-                    grid[i][j] = King(j, i, 'w')
+                    grid[i][j] = King(i, j, 'w')
                 elif(grid[i][j] == 'k'):
-                    grid[i][j] = King(j, i, 'b')
+                    grid[i][j] = King(i, j, 'b')
 
-                    
+
+    # takes in which color's turn it is, generates all valid moves for that player
+    def generateMoves(self, color):
+        if(color == 'b'):
+            GucciMane = 1 # just here as a temporary placeholder so no errors are thrown
+            # STUB - search the entire grid for black pieces, call move() for them
+            # then check to see if they are valid coordinate moves for that piece
+
+        if(color == 'w'):
+            GucciMane = 1 # just here as a temporary placeholder so no errors are thrown
+            # STUB
+
+
--- piece.py
+++ piece.py
@@ -2,57 +2,70 @@
 from abc import ABC, abstractmethod     # for abstract classes
   
 class Piece(ABC):
-    def __init__(self, x = 0 , y = 0, color = 'b'): # STUB!! get rid of default values later
-        self.x = x
-        self.y = y
+    def __init__(self, i = 0 , j = 0, color = 'b'): # STUB!! get rid of default values later
+        self.i = i
+        self.j = j
         self.color = color
         print("oh yeah we a piece man")
 
+
+    # this method returns a list of coordinates that a piece would be able to move to
+    # from its position if the rest of the board was completely empty
     def move(self):
         pass
 
 class Pawn(Piece):
+    def __init__(self):
+        self.id = 'P'
+
+    # this method returns a list of coordinates that a Pawn would be able to move to
+    # from its position if the rest of the board was completely empty
     def move(self):
-        coordsList = []
+
+        coordsList = []   # this is a list of (i,j) coordinates where a pawn can move.
+                          # each move() method will need a list like this.  For a pawn, the list
+                          # is only going to have one pair of coordinates, but different
+                          # pieces will have a lot more coordinates
 
         # black pawn
         if(self.color == 'b'):
-            if(self.y != 7):
-                coordsList.append([self.x, self.y + 1])
+            if(self.i != 7): # this line checks to make sure a black pawn isn't at the very bottom of the board
+                coordsList.append([self.i, self.j + 1]) # this is the syntax for adding one particular coordinate to the list
+                                                        # 'self.i' and 'self.j' are instance variables for this pawn
+                                                        # the [INT, INT] is just a pair of coordinates.  For some reason,
+                                                        # you can just shove this into a list in python and it magically works
 
         # white pawn
         else:
-            if(self.y != 0):
-                coordsList.append([self.x, self.y - 1])
+            if(self.i != 0): # this line checks to make sure a white pawn isn't at the very top of the board
+                coordsList.append([self.i, self.j - 1])
 
-        return(coordsList)
+        return(coordsList)  
         print("Stub: Move Pawn")
 
 class Rook(Piece):
     def move(self):
+        coordsList = []
         print("Stub: Move Rook")
 
 class Knight(Piece):
     def move(self):
+        coordsList = []
         print("Stub: Move Knight")
 
 class Bishop(Piece):
     def move(self):
+        coordsList = []
         print("Stub: Move Bishop")
 
 class Queen(Piece):
     def move(self):
+        coordsList = []
         print("Stub: Move Queen")
 
 class King(Piece):
-    # def __init__(self):
-
     def move(self):
+        coordsList = []
         print("Stub: Move King")
 
 
-
-# thing = Piece()
-# thing.move()
-# thing2 = King()
-# thing2.move()

2e4306f56841c02915c9723e5839a2f7b6df0fdf
MatthewJohnson257 <MJohnson5342@gmail.com>
2020-02-18 11:13:00 -0500

    Command line input for which initial board we want

--- chess.py
+++ chess.py
@@ -3,6 +3,8 @@
 from piece import Piece, Pawn, Rook, Knight, Bishop, Queen, King
 from board import Board
 from tkinter import *
+import argparse         # used to process comand line inputs
+import sys              # used to print error messages
 import copy
 
 
@@ -24,7 +26,7 @@ inputFull = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
 ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
 
-inputDefault = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+inputTest1 = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ['p', 'p', 'N', 'p', 'p', 'p', '_', 'p'],
 ['P', '_', '_', 'P', '_', '_', 'R', '_'],
 ['_', '_', 'Q', 'B', 'N', '_', 'p', '_'],
@@ -33,7 +35,7 @@ inputDefault = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ['_', 'P', 'P', '_', 'P', 'P', 'P', '_'],
 ['R', '_', 'B', '_', '_', '_', '_', '_']]
 
-inputTest1 = [['_', '_', '_', '_', 'k', '_', '_', '_'],
+inputTest2 = [['_', '_', '_', '_', 'k', '_', '_', '_'],
 ['_', '_', '_', '_', '_', '_', '_', '_'],
 ['_', '_', '_', '_', '_', '_', '_', '_'],
 ['_', '_', '_', '_', '_', '_', '_', '_'],
@@ -214,12 +216,31 @@ def graphicalBoard(board):
 #                                    GLOBAL                                   #
 ###############################################################################
 
-# boardA = Board(inputA, True)
-# boardA.printBoard()
-# boardA.graphicalBoard()
+# command line argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument('Arguments', metavar='N', type=int, nargs='+')
+arguments = parser.parse_args()
+
+# error messages
+if(len(arguments.Arguments) != 1):
+    sys.exit("    Error: Incorrect number of command line arguments supplied; 1 needed")
+
+usedBoard = arguments.Arguments[0]             # which given puzzle to solve
+
+inputGrid = None
+if(usedBoard == 1):
+    inputGrid = inputA
+elif(usedBoard == 2):
+    inputGrid = inputFull
+elif(usedBoard == 3):
+    inputGrid = inputTest1
+elif(usedBoard == 4):
+    inputGrid = inputTest2
 
 
-boardDefault = Board(inputTest1, True)
+
+
+boardDefault = Board(inputGrid, True)
 boardDefault.printBoard()
 graphicalBoard(boardDefault)
 
